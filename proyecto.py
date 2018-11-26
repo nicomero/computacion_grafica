c264161eb488f5bb158084799bd800e43dc37e49
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2018-11-26 09:54:03 -0300

    poner superficies

--- proyecto.py
+++ proyecto.py
@@ -4,14 +4,14 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-#    e-------f
+#    a-------b
 #   /|      /|
 #  / |     / |
-# a-------b  |
+# e-------f  |
 # |  |    |  |
-# |  g----|--h
+# |  c----|--d
 # | /     | /
-# c-------d
+# g-------h
 
 vertices = {}
 vertices['a'] = [-1, 1, -1]
@@ -41,8 +41,23 @@ edges = (
     ("f","h")
     )
 
+surfaces = [
+    ("g","h","f","e"),
+    ("e","f","b","a"),
+    ("h","d","b","f")
+]
+
+surfToMap = [
+    (0,0),
+    (1,0),
+    (1,1),
+    (0,1)
+]
+
+mode = True
+
 def checkKeyPressed(event):
-    global currentV
+    global currentV, mode
 
     if event.key == pygame.K_ESCAPE:
         pygame.quit()
@@ -65,20 +80,55 @@ def checkKeyPressed(event):
     elif event.key == pygame.K_TAB:
         currentV = (currentV+1)%len(vNames)
 
+    elif event.key == pygame.K_p:
+        mode = not mode
+
+def loadTexture():
+    textureSurface = pygame.image.load('test.png')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
 
 
-def Cube():
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
-    glEnd()
+def Cube(lines=True):
+    if lines:
+        glBegin(GL_LINES)
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+    else:
+        glBegin(GL_QUADS)
+        for surface in surfaces:
+
+            index=0
+            for vertex in surface:
+                glTexCoord2f(surfToMap[index][0], surfToMap[index][1])
+                glVertex3fv(vertices[vertex])
+                index += 1
+        glEnd()
 
 
 def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    loadTexture()
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
@@ -94,7 +144,7 @@ def main():
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-        Cube()
+        Cube(mode)
         pygame.display.flip()
         pygame.time.wait(10)
 
